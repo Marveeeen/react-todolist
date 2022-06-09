fb5cfd7f84042171d7d64b9a1b14554639e4e211
unknown <jmvroqueee@gmail.com>
2022-06-09 13:01:22 +0800

    Added style and Format

--- src/App.js
+++ src/App.js
@@ -1,7 +1,61 @@
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks]  = useState([
+    {
+      id: 1,
+      done: false,
+      task: "Tasks 1"
+    },
+    {
+      id: 2,
+      done: false,
+      task: "Tasks 2"
+    },
+    {
+      id: 3,
+      done: false,
+      task: "Tasks 3"
+    },
+  ])
+
+  const today = new Date();
+  
+  const handleChecked = (id) => {
+    console.log(id)
+    const taskLists = tasks.map(task =>
+      task.id === id ? {...task, done: !task.done} : task 
+    );
+
+    setTasks(taskLists)
+  }
+  
   return (
     <div className="App">
+      <header>
+        <h1>Tasks Management</h1>
+      </header>
+      <main>
+        {tasks.length ? (
+          <ul>
+            {tasks.map(task => (
+              <li key={task.id}>
+                <input 
+                  type="checkbox"
+                  checked={task.done}
+                  onChange={() => handleChecked(task.id)}
+                  />
+                <label>{task.task}</label>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>Your Task is empty.</p>
+        )}
+      </main>
+      <footer>
+          <p>Copyrigt &copy; {today.getFullYear()}</p>
+      </footer>
     </div>
   );
 }
